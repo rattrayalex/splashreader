704e8188436c8518d1c92b47360cb506972db7bf
Alex Rattray <rattray.alex@gmail.com>
2014-11-03 21:41:47 +1300

    build/deploy on Mon Nov 03 2014 21:41:47 GMT+1300 (NZDT)

---
+++ js/chrome.js
@@ -0,0 +1,58 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./coffee/chrome.coffee":[function(require,module,exports){
+var getCurrentUrl, insertIframe, main, toggleIframe;
+
+alert('hello world');
+
+getCurrentUrl = function() {
+  return document.URL;
+};
+
+insertIframe = function(url) {
+  var iframe;
+  iframe = document.createElement('iframe');
+  iframe.style.position = 'fixed';
+  iframe.style.top = 0;
+  iframe.style.left = 0;
+  iframe.style.right = 0;
+  iframe.style.bottom = 0;
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
+  iframe.style.margin = 0;
+  iframe.style.padding = 0;
+  iframe.style.overflow = 'hidden';
+  iframe.style.zIndex = 999999;
+  iframe.style.display = 'none';
+  iframe.setAttribute('src', 'http://www.splashreaderapp.com/#' + url);
+  document.body.appendChild(iframe);
+  return iframe;
+};
+
+toggleIframe = function(iframe) {
+  if (iframe.style.display === 'none') {
+    return iframe.style.display = 'block';
+  } else {
+    return iframe.style.display = 'none';
+  }
+};
+
+main = function() {
+  var url;
+  if (window.SplashReader == null) {
+    window.SplashReader = {
+      iframe: null
+    };
+  }
+  if (!window.SplashReader.iframe) {
+    url = getCurrentUrl();
+    alert('url is' + url);
+    window.SplashReader.iframe = insertIframe(url);
+  }
+  return toggleIframe(window.SplashReader.iframe);
+};
+
+main();
+
+
+
+},{}]},{},["./coffee/chrome.coffee"]);
